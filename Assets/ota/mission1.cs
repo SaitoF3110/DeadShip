773a8146ba0ddadc127cab38c1ef079695b39f7e
HIROMARU18 <vantanhiro@gmail.com>
2023-07-16 16:49:19 +0900

    mission１途中経過

--- Assets/ota/mission1.cs
+++ Assets/ota/mission1.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class mission1 : MonoBehaviour
 {
+
+    public float requiredHoldTime = 10.0f;
+
+    private bool isHolding = false;
+    private float holdStartTime = 0.0f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    
+     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            isHolding = true;
+            holdStartTime = Time.time;
+        }
+    
+
+        if (Input.GetKeyUp(KeyCode.M) || (isHolding && Time.time - holdStartTime >= requiredHoldTime))
+        {
+            
+            if (isHolding)
+            {
+                Debug.Log("defused!");
+            }
+
+            isHolding = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.M)) 
+        {
+           if(!isHolding)
+            {
+
+            }
+        }
     }
 }
